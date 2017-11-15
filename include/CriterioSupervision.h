39d11d63fea4e2de1fb05fc8c2642ad96d315bea
danielx25 <danielx-25@hotmail.com>
2017-11-14 22:43:37 -0300

    arreglando un pequeño error

--- include/CriterioSupervision.h
+++ include/CriterioSupervision.h
@@ -11,7 +11,7 @@ class CriterioSupervision
         static bool criterioMp10(double *entrada, double *neurona, int &subclase)
         {
             int mp10_entrada = entrada[Configuracion::NUMERO_ENTRADAS-1]*800;
-            int mp10_neurona = neurona[Configuracion::NUMERO_ENTRADAS-1];
+            int mp10_neurona = neurona[Configuracion::NUMERO_ENTRADAS-1]*800;
             int rangos[16] = {50, 100, 150, 183, 216, 250, 283, 316, 350, 400, 450, 500, 550, 600, std::numeric_limits<int>::infinity()};
             int subclaseEntrada = -1;
             int subclaseNeurona = -1;
