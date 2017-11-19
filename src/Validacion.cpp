ab584c601882e2d3d291e332e63475e4d7232ad8
danielx25 <danielx-25@hotmail.com>
2017-11-19 12:54:43 -0300

    mejorando las estadisticas

--- src/Validacion.cpp
+++ src/Validacion.cpp
@@ -7,7 +7,7 @@ Validacion::Validacion()
     neurona = new double[Configuracion::NUMERO_ENTRADAS];
     numeroAciertos = 0;
     acertividad = 0;
-    radioMp10 = 0.0625;//0.0625;//0.125;
+    radioMp10 = 0.125;//0.0625;//0.125;
 }
 
 Validacion::~Validacion()
@@ -65,6 +65,13 @@ void Validacion::iniciarValidacion()
     double mp10Inferior = 0;
     double mp10Superior = 0;
     numeroAciertos = 0;
+
+    int *arregloNeurona = new int[Configuracion::NUMERO_NEURONAS];
+    for(int i=0; i<Configuracion::NUMERO_NEURONAS; i++)
+        arregloNeurona[i]=0;
+
+    int numeroAlertasM = 0;
+    int numeroAciertosM = 0;
     for(int indice = 0; indice < Configuracion::NUMERO_DATOS; indice++)
     {
         Arreglos::getFila(entrada, datosEntrenamiento, indice);
@@ -74,7 +81,7 @@ void Validacion::iniciarValidacion()
         mp10Real = entrada[Configuracion::NUMERO_ENTRADAS-1];
         mp10Inferior = mp10Predecion - radioMp10;
         mp10Superior = mp10Predecion + radioMp10;
-
+        arregloNeurona[ganador]+=1;
 
         //printf(" %f < %f &&  %f >= %f\n", mp10Inferior, mp10Real, mp10Superior, mp10Real);
         if(mp10Inferior<mp10Real && mp10Superior>mp10Real)
@@ -83,8 +90,24 @@ void Validacion::iniciarValidacion()
             //printf("predecion: %f ---- real: %f \n", mp10Predecion, mp10Real);
         }
 
+        if(mp10Real>0.1875)
+        {
+            numeroAlertasM +=1;
+            if(mp10Inferior<mp10Real && mp10Superior>mp10Real)
+                numeroAciertosM+=1;
+        }
+
         //printf("predecion: %f ---- real: %f \n", mp10Predecion, mp10Real);
     }
     printf("numero aciertos: %d\n", numeroAciertos);
+    printf("numero alertas mayores   : %d\n", numeroAlertasM);
+    printf("numero aciertos alertas M: %d\n", numeroAciertosM);
 
+    int numeroNeuronasActivas = 0;
+    for(int i=0; i<Configuracion::NUMERO_NEURONAS; i++)
+    {
+        if (arregloNeurona[i] > 0 )
+            numeroNeuronasActivas+=1;
+    }
+    printf("numero neuronas activas: %d\n", numeroNeuronasActivas);
 }
